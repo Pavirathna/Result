0e41bac20c6075c0a8bd899eb27c4b15493c6874
pavirathna <pvthra027@gmail.com>
2019-05-23 00:37:24 +0530

    leap year in switch statement

--- src/NumbersOfDaysInMonth.java
+++ src/NumbersOfDaysInMonth.java
@@ -1,30 +1,55 @@
 import java.util.Scanner;
 
-    public class NumbersOfDaysInMonth {
+public class NumbersOfDaysInMonth {
 
-        public static void main(String[] args) {
+    public static void main(String[] args) {
 
-                Scanner s1 = new Scanner(System.in);
-                System.out.println("Enter the year");
-                int year = s1.nextInt();
-            System.out.println(isLeapYear(year));
-            }
-
-        private static boolean isLeapYear(int year) {
-            if ((year % 4 == 0) &&( year>0&&year<9999)){
-                if ((year % 100 == 0)) {
-                    if (year % 400 != 0) {
-                          return false;
-                    }
-                }
-                 return  true;
-                 }
-                 else
-                {
-                  return false;
-                }
-        }
+        Scanner s1 = new Scanner(System.in);
+        System.out.println("Enter the year");
+        int year = s1.nextInt();
+        System.out.println(isLeapYear(year));
+        System.out.println(isLeapYear(2,2000));
     }
+
+    public static boolean isLeapYear(int year) {
+        boolean check=false;
+        if ((year % 4 == 0) &&( year>0&&year<9999)){
+            if ((year % 100 == 0)) {
+                if (year % 400 != 0) {
+                    //return check;
+                    return false;
+                }
+            }
+            check=true;             //return  true;
+        }
+        return check;   // return false;
+    }
+
+    public static int  isLeapYear(int Month,int Year)
+    {
+        int m=Month;
+        switch (m)
+        {
+            case 2:
+                if (isLeapYear(Year))
+                {
+                    return 29;
+                }
+                return 28;
+            break;
+            case 1: case 3: case 5: case 7: case 8: case 10:case 12:
+            return 31;
+            break;
+
+            case 4: case 6: case 9: case 11:
+            return 30;
+            break;
+            default:
+                return -1;
+        }
+
+    }
+  }
 
 
 
